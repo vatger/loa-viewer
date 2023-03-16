3279bc42d3049ce6b91b1b181108b60bdfe8bb25
Moritz <moritz.frenken@gmail.com>
2023-03-16 15:21:30 +0100

    imprint in footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -12,7 +12,16 @@ const Footer = (props: any) => {
 
     return (
         <>
-            <div className="footer">VATGER LoA Viewer. Version {version}</div>
+            <div className="footer">
+                <div className="flex justify-content-between">
+                    <span className="flex align-items-start ml-2">VATGER LoA Viewer. Version {version}</span>
+                    <span className="flex align-items-center mr-2">
+                        <a href="https://vatsim-germany.org/imprint" target="_blank" style={{ textDecoration: 'none' }}>
+                            Imprint
+                        </a>
+                    </span>
+                </div>
+            </div>
         </>
     );
 };
