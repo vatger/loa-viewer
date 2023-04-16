67c4e846497fe892d05d5722e808889763b0d00b
Moritz <moritz.frenken@gmail.com>
2023-04-16 18:24:17 +0200

    fixed url

--- backend/src/services/stationMappings.service.ts
+++ backend/src/services/stationMappings.service.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function getStationMappings(): Promise<any> {
     try {
-        const response = await axios.get('https://git.vatsim-germany.org/nav-public/loa-station-mapping/-/raw/main/station_mapping.json');
+        const response = await axios.get('http://git.vatsim-germany.org/nav-public/loa-station-mapping/-/raw/main/station_mapping.json');
 
         return response.data;
     } catch (error) {
