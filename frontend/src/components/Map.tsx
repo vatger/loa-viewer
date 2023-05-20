ec42a7a61a1c2b904e03cf34f00936863a8bdd53
Leon K <leon.kleinschmidt@outlook.de>
2023-05-20 16:51:31 +0200

    add search bar

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,12 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, MapContainerProps, TileLayerProps } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { FrontendCondition } from 'interfaces/condition.interface';
+import { InputText } from 'primereact/inputtext';
 
 export default function Map() {
+    const [conditions, setConditions] = useState<FrontendCondition[]>([]);
+    const [filteredConditions, setFilteredConditions] = useState<Record<string, FrontendCondition[]>>();
+    const [search, setSearch] = useState<string>('GIN');
+
+    useEffect(() => {
+        // set default search if nothing is inputted
+        if (search === '') {
+            setSearch('GIN');
+        }
+    }, [search]);
 
     return (
         <>
             <div style={{ position: 'relative' }}>
+                <InputText
+                    type="search"
+                    placeholder="Search"
+                    onChange={e => setSearch(e.target.value)}
+                    style={{
+                        position: 'absolute',
+                        top: '5%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        zIndex: 1,
+                    }}
+                />
                 <MapContainer center={[50.026292, 8.765245]} zoom={8} style={{ width: '100vw', height: '100vh', zIndex: 0 }} maxZoom={10} minZoom={6}>
                     <TileLayer
                         url="https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png"
