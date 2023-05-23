16de8ecd09046cc590ec1bb6f8f7df0c243c32c3
Leon K <leon.kleinschmidt@outlook.de>
2023-05-23 23:50:55 +0200

    add navaids filename finder

--- backend/src/services/aipData.service.ts
+++ backend/src/services/aipData.service.ts
@@ -17,7 +17,7 @@ async function getMetadata(): Promise<any> {
  * @param {string} type - The type of data which should be returned, must be either 'AIXM 5.1' (i.e. xml) or 'Excel'
  * @returns the current xml filename containing the waypoint data of the current airac
  */
-async function getWaypointDataFilename(type: string): Promise<string | undefined> {
+async function getWaypointDataFilename(type: string): Promise<{ waypointsFilename: string | undefined; navaidsFilename: string | undefined } | undefined> {
     if (type !== 'Excel' && type !== 'AIXM 5.1') {
         console.error(`${getWaypointDataFilename.name} Insufficient type`);
         return undefined;
@@ -26,18 +26,17 @@ async function getWaypointDataFilename(type: string): Promise<string | undefined
     try {
         // retrieve metadata json
         const metadata = await getMetadata();
+        const waypointsFilename = metadata?.Amdts?.find(amdt => amdt?.Amdt === 9999)
+            ?.Metadata?.datasets?.find(dataset => dataset?.name === 'AIP data set')
+            ?.items?.find(item => item?.name === 'ED Waypoints')
+            ?.releases?.find(release => release?.type === type)?.filename;
 
-        // Find snapshot amendment (i.e., most recent, currently effective data)
-        const Amendment = metadata?.Amdts?.find((amdt: any) => amdt?.Amdt === 9999);
+        const navaidsFilename = metadata?.Amdts?.find(amdt => amdt?.Amdt === 9999)
+            ?.Metadata?.datasets?.find(dataset => dataset?.name === 'AIP data set')
+            ?.items?.find(item => item?.name === 'ED Navaids')
+            ?.releases?.find(release => release?.type === type)?.filename;
 
-        // route through json to find the filename:
-        const AmdtDatasets = Amendment?.Metadata?.datasets;
-        const waypointDataset = AmdtDatasets?.find((dataset: any) => dataset?.name === 'AIP data set');
-        const waypointItem = waypointDataset?.items?.find((item: any) => item?.name === 'ED Waypoints');
-        const waypointRelease = waypointItem?.releases?.find((release: any) => release?.type === type);
-        const filename = waypointRelease?.filename;
-
-        return filename;
+        return { waypointsFilename, navaidsFilename };
     } catch (error) {
         console.error(`Error at ${getWaypointDataFilename.name}`);
         console.error(error);
