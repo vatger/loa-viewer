596af74af4d61db516ecf708c847753db88a308f
Leon K <leon.kleinschmidt@outlook.de>
2023-05-27 22:34:38 +0200

    add sector controller

---
+++ backend/src/controllers/sector.controller.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from 'express';
+import sectorService from 'services/sector.service';
+import { SectorDocument } from 'models/sector.model';
+
+export async function getAllWaypoints(req: Request, res: Response, next: NextFunction) {
+    try {
+        const sectors: SectorDocument[] | undefined = await sectorService.getAllSectors();
+
+        res.json(sectors);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export default {
+    getAllWaypoints,
+};
