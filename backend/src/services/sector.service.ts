24a9b8d38705bf5eafde1bfed7de3b79e872da93
Leon K <leon.kleinschmidt@outlook.de>
2023-05-27 22:33:56 +0200

    Add retrieveAirspacesFromCountries function

--- backend/src/services/sector.service.ts
+++ backend/src/services/sector.service.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Airspace from '@shared/interfaces/sector.interface';
 
 async function getSectorData(filename: string): Promise<any> {
     try {
@@ -11,6 +12,31 @@ async function getSectorData(filename: string): Promise<any> {
     }
 }
 
+async function retrieveAirspacesFromCountries(countries: string[]): Promise<Airspace[]> {
+    try {
+        const airspaces: Airspace[] = [];
+
+        for (const country of countries) {
+            const data = await getSectorData(country);
+            const airspacedata = data.airspace;
+
+            if (airspacedata) {
+                for (const airspace of airspacedata) {
+                    airspaces.push(airspace);
+                }
+            }
+        }
+
+        return airspaces;
+    } catch (error) {
+        console.error(`Error while at ${retrieveAirspacesFromCountries.name}`);
+        console.error(error);
+    }
+
+    return [];
+}
+
 export default {
     getSectorData,
+    retrieveAirspacesFromCountries,
 };
